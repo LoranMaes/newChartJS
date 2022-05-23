2aeb882412385796a64f6b06f0e168bcfc8de979
Loran Maes <75251173+LoranMaes@users.noreply.github.com>
2022-05-23 15:36:03 +0200

    All the charts + a cute snail =)

--- app.js
+++ app.js
@@ -7,11 +7,306 @@ import Chart from "chart.js/auto";
             const array = data
             scoresPerStudentWisk(array)
             scoresPerGroep(array)
+            geslaagden(array)
+            manVrouw(array)
+            bestGroup(array)
         })
         .catch(err => console.error(err))
     }
     fetcher()
     
+    const bestGroup = function(array){
+        //The group is to see who passed every test
+        //The counter is to see how many people were in the group in order to calculate %
+        let groupA = 0
+        let aCounter = 0
+
+        let groupB = 0
+        let bCounter = 0
+        
+        let groupC = 0
+        let cCounter = 0
+
+        let groupD = 0
+        let dCounter = 0
+
+        let groupE = 0
+        let eCounter = 0
+
+        array.forEach(element => {
+            const math = parseInt(element.math)
+            const reading = parseInt(element.reading)
+            const writing = parseInt(element.writing)
+            const group = element.group
+
+            if(group === "group A"){
+                if(math >= 50){
+                    if(reading >= 50){
+                        if(writing >= 50){
+                            groupA++
+                        }
+                    }
+                }
+                aCounter++
+            }
+
+            else if(group === "group B"){
+                if(math >= 50){
+                    if(reading >= 50){
+                        if(writing >= 50){
+                            groupB++
+                        }
+                    }
+                }
+                bCounter++
+            }
+
+            else if(group === "group C"){
+                if(math >= 50){
+                    if(reading >= 50){
+                        if(writing >= 50){
+                            groupC++
+                        }
+                    }
+                }
+                cCounter++
+            }
+
+            else if(group === "group D"){
+                if(math >= 50){
+                    if(reading >= 50){
+                        if(writing >= 50){
+                            groupD++
+                        }
+                    }
+                }
+                dCounter++
+            }
+
+            else if(group === "group E"){
+                if(math >= 50){
+                    if(reading >= 50){
+                        if(writing >= 50){
+                            groupE++
+                        }
+                    }
+                }
+                eCounter++
+            }
+        })
+
+        //Put everything in percentage
+        groupA = groupA / aCounter * 100
+        groupB = groupB / bCounter * 100
+        groupC = groupC / cCounter * 100
+        groupD = groupD / dCounter * 100
+        groupE = groupE / eCounter * 100
+
+        const fifthChart = document.querySelector("#chartFive")
+
+        const labels = [
+            "Group A",
+            "Group B",
+            "Group C",
+            "Group D",
+            "Group E"
+        ]
+
+        const data = {
+            labels: labels,
+            datasets: [{
+                label: `Percentage of people in the group who passed all their tests`,
+                backgroundColor: [
+                    "rgb(255, 99, 132)",
+                    "rgb(252, 255, 99)",
+                    "rgb(99, 148, 255)",
+                    "rgb(61, 199, 130)",
+                    "rgb(204, 145, 36)",
+                    "rgb(144, 228, 34)"
+                ],
+                borderColor: [
+                    "rgb(255, 99, 132)",
+                    "rgb(252, 255, 99)",
+                    "rgb(99, 148, 255)",
+                    "rgb(61, 199, 130)",
+                    "rgb(204, 145, 36)",
+                    "rgb(144, 228, 34)"
+                ],
+                data: [
+                    groupA,
+                    groupB,
+                    groupC,
+                    groupD,
+                    groupE
+                ]
+            }]
+        }
+
+        const config = {
+            type: "polarArea",
+            data: data
+        }
+
+        const myChart = new Chart(
+            fifthChart,
+            config
+        )
+
+    }
+
+    //Mannen vs vrouwen geslaagd
+    const manVrouw = function(array){
+        let vrouwenWisk = 0
+        let vrouwenSchrijf = 0
+        let vrouwenLees = 0
+
+        let mannenWisk = 0
+        let mannenSchrijf = 0
+        let mannenLees = 0
+
+        array.forEach(element => {
+            if(element.gender === "female"){
+                if(parseInt(element.math) >= 50){
+                    vrouwenWisk++
+                }
+                if(parseInt(element.reading) >= 50){
+                    vrouwenSchrijf++
+                }
+                if(parseInt(element.writing) >= 50){
+                    vrouwenLees++
+                }
+            }
+            else if(element.gender === "male"){
+                if(parseInt(element.math) >= 50){
+                    mannenWisk++
+                }
+                if(parseInt(element.reading) >= 50){
+                    mannenSchrijf++
+                }
+                if(parseInt(element.writing) >= 50){
+                    mannenLees++
+                }
+            }
+        })
+
+        const fourthChart = document.querySelector("#chartFour")
+        
+        const labels = [
+            "Woman passed math",
+            "Woman passed reading",
+            "Woman passed Writing",
+            "Man passed math",
+            "Man passed reading",
+            "Man passed Writing"
+        ]
+
+        const data = {
+            labels: labels,
+            datasets: [{
+                label: "Amount of men vs women who passed the subjects",
+                backgroundColor: [
+                    "rgb(255, 99, 132)",
+                    "rgb(230, 73, 107)",
+                    "rgb(197, 46, 79)",
+                    "rgb(99, 148, 255)",
+                    "rgb(65, 115, 221)",
+                    "rgb(37, 83, 184)"
+                ],
+                borderColor: [
+                    "rgb(255, 99, 132)",
+                    "rgb(230, 73, 107)",
+                    "rgb(197, 46, 79)",
+                    "rgb(99, 148, 255)",
+                    "rgb(65, 115, 221)",
+                    "rgb(37, 83, 184)"
+                ],
+                data: [
+                    vrouwenWisk,
+                    vrouwenLees,
+                    vrouwenSchrijf,
+                    mannenWisk,
+                    mannenLees,
+                    mannenSchrijf
+                ]
+            }]
+        }
+
+        const config = {
+            type: "polarArea",
+            data: data
+        }
+
+        const myChart = new Chart(
+            fourthChart,
+            config
+        )
+    }
+
+    //Tel personen met meer dan 50% voor de 3 vakken (die dus geslaagd zijn)
+    const geslaagden = function(array){
+        let wiskGeslaagd = 0
+        let lezenGeslaagd = 0
+        let schrijvenGeslaagd = 0
+
+        array.forEach(element => {
+            const math_score = parseInt(element.math)
+            const reading_score = parseInt(element.reading)
+            const writing_score = parseInt(element.writing)
+
+            if(math_score >= 50){
+                wiskGeslaagd++
+            }
+            if(reading_score >= 50){
+                lezenGeslaagd++
+            }
+            if(writing_score >= 50){
+                schrijvenGeslaagd++
+            }
+        })
+
+        const thirdChart = document.querySelector("#chartThree")
+        const header = document.querySelector("#total-students")
+        header.innerHTML = `Total amount of students who passed the different subjects<br><span class="header-total-students">Total students: ${array.length}</span>`
+
+        const labels = [
+            "Passed math",
+            "Passed reading",
+            "Passed writing"
+        ]
+
+        const data = {
+            labels: labels,
+            datasets: [{
+                label: `Students who passed math, reading and writing. Total Students: ${array.length}`,
+                backgroundColor: [
+                    "rgb(255, 99, 132)",
+                    "rgb(99, 148, 255)",
+                    "rgb(252, 255, 99)"
+                ],
+                borderColor: [
+                    "rgb(255, 99, 132)",
+                    "rgb(99, 148, 255)",
+                    "rgb(252, 255, 99)"
+                ],
+                data: [
+                    wiskGeslaagd,
+                    lezenGeslaagd,
+                    schrijvenGeslaagd
+                ]
+            }]
+        }
+
+        const config = {
+            type: "doughnut",
+            data: data
+        }
+
+        const myChart = new Chart(
+            thirdChart,
+            config
+        )
+    }
+
     //Scores van wiskunde voor alle studenten
     const scoresPerStudentWisk = function(array){
         let wiskundeScores = []
@@ -40,7 +335,14 @@ import Chart from "chart.js/auto";
         const config = {
             type: "bar",
             data: data,
-            options: {}
+            options: {
+                scales: {
+                  y: { // defining min and max so hiding the dataset does not change scale range
+                    min: 0,
+                    max: 100
+                  }
+                }
+          }
         }
 
         const myChart = new Chart(
